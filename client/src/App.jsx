8ad95f58412b28146879729ae6238eaf9432b3e3
Shree2124 <shreealasande@gmail.com>
2024-12-02 23:41:01 +0530

    fix: routing error fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,19 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage, LoginPage, RegisterPage } from "./pages/index.js";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
 import { useThemeContext } from "./context/ThemeContext";
+import { HomePage } from "./pages";
 
 function App() {
   const { theme } = useThemeContext();
+
+ 
 
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-          </Routes>
-        </BrowserRouter>
+        <HomePage/>
       </ThemeProvider>
     </>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,37 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { HomePage, LoginPage, RegisterPage } from "./pages/index.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import { useThemeContext } from "./context/ThemeContext";
+import App from "./App.jsx";
+
+const router = createBrowserRouter([
+  {
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <CssBaseline />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>
 );
