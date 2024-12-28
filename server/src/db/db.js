4214d19ebc22d78a0e3395d8344a695b2de94088
Shree2124 <shreealasande@gmail.com>
2024-12-28 22:01:51 +0530

    test: deployment testing

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -3,7 +3,7 @@ import { db_name, mongodb_url } from "../config/variables.js";
 const dbConnection = async () => {
     try {
         console.log(`${mongodb_url}/${db_name}`);
-        const connection = await mongoose.connect(`${mongodb_url}/${db_name}`)
+        const connection = await mongoose.connect(`${process.env.MONGODB_URl}/${db_name}`)
         console.log("db connected successfully");
     } catch (error) {
         console.log(error);
