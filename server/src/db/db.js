c046917fb4b54d7353df1c22951d47a9b0fdbbce
Shree2124 <shreealasande@gmail.com>
2024-12-28 22:09:35 +0530

    test: deployment testing

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -3,7 +3,11 @@ import { db_name, mongodb_url } from "../config/variables.js";
 const dbConnection = async () => {
     try {
         console.log(`${mongodb_url}/${db_name}`);
-        const connection = await mongoose.connect(`${process.env.MONGODB_URl}/${db_name}`)
+        const connection = await mongoose.connect(`${process.env.MONGODB_URl}/${db_name}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000
+        })
         console.log("db connected successfully");
     } catch (error) {
         console.log(error);
